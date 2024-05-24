c21d79c3dc95d32d0ac8b83f5e75c3996ae108fc
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-05-24 20:59:25 +0300

    add NotFoundPage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 // import './App.module.css'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from '../../pages/HomePage/HomePage'
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
  
 export default function App () {
     return (
       <div>
         <Routes>
-                <Route path="/" element={<HomePage />}></Route>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
         </div>
     )
