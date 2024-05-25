6005c0e7eb437454221402e5b1fb5f7a4073eea3
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-05-25 20:27:04 +0300

    added requests

--- src/movies-api.js
+++ src/movies-api.js
@@ -11,10 +11,48 @@ export const fetchTrendingMovies = async () => {
   const url = 'https://api.themoviedb.org/3/trending/movie/day';
   try {
     const response = await axios.get(url, options);
-    console.log(response.data.results);
     return response.data.results;
     } catch (error) {
     console.error(error);
   }
 };
 
+export const fetchSearchMovie = async (query) => {
+  const url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=false&language=en-US&page=1`;
+  try {
+    const response = await axios.get(url, options);
+    return response.data.results;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const fetchMovieDetails = async (movieId) => {
+  const url = `https://api.themoviedb.org/3/movie/${movieId}`;
+  try {
+    const response = await axios.get(url, options);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const fetchMovieCredits = async (movieId) => {
+  const url = `https://api.themoviedb.org/3/movie/${movieId}/credits`;
+  try {
+    const response = await axios.get(url, options);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const fetchMovieReviews = async (movieId) => {
+  const url = `https://api.themoviedb.org/3/movie/${movieId}/reviews`;
+  try {
+    const response = await axios.get(url, options);
+    return response.data.results;
+  } catch (error) {
+    console.error(error);
+  }
+};
