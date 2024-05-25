48c9970ad0bb9258f1f00dbe848b35d326c8dd71
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-05-25 18:22:20 +0300

    add navigation

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 // import './App.module.css'
 import { Routes, Route } from 'react-router-dom'
+import Navigation from '../Navigation/Navigation';
 import HomePage from '../../pages/HomePage/HomePage'
 import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
  
 export default function App () {
     return (
       <div>
+        <Navigation />
         <Routes>
           <Route path="/" element={<HomePage />}></Route>
           <Route path="*" element={<NotFoundPage />} />
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,2 +1,18 @@
+import { NavLink } from 'react-router-dom';
+import css from './Navigation.module.css';
 
+export default function Navigation() {
+  return (
+    <nav>
+      <ul className={css.menu}>
+        <li>
+          <NavLink to="/" className={css.link} activeClassName='active'>Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/movies" className={css.link} activeClassName='active'>Movies</NavLink>
+        </li> 
+      </ul>
+    </nav>
+  );
+}
 
