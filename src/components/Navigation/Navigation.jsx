4ce7afe8726a78a876f59db733e4ab5e083278b3
Ostrinka <148815239+Ostrinka@users.noreply.github.com>
2024-05-28 21:18:11 +0300

    add isActive

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,15 +1,21 @@
 import { NavLink } from 'react-router-dom';
+import clsx from "clsx";
 import css from './Navigation.module.css';
+
+function getLink({isActive}){
+  return clsx(css.link, isActive && css.active)
+}
+
 
 export default function Navigation() {
   return (
     <nav>
       <ul className={css.menu}>
         <li>
-          <NavLink to="/" className={css.link}>Home</NavLink>
+          <NavLink to="/" className={getLink}>Home</NavLink>
         </li>
         <li>
-          <NavLink to="/movies" className={css.link}>Movies</NavLink>
+          <NavLink to="/movies" className={getLink}>Movies</NavLink>
         </li> 
       </ul>
     </nav>
